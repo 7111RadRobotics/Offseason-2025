fc686b4188137f6a3b4288899f66dce7c06b9ea7
Seamaj <seamajesty1st@gmail.com>
2025-07-15 16:03:51 -0500

    def professional

--- Library/src/main/java/frc/lib/pathfinding/Waypoint.java
+++ Library/src/main/java/frc/lib/pathfinding/Waypoint.java
@@ -3,17 +3,33 @@ package frc.lib.pathfinding;
 import edu.wpi.first.math.geometry.Pose2d;
 
 public class Waypoint {
-        Pose2d pose = pos;
-        double translationTolerance = transTolerance;
-        double rotationTolerance = rotTolerance;
-        double maxTranslationSpeed = maxTransSpeed;
-        double maxRotationSpeed = maxRotSpeed;
+    Pose2d pose;
+    double translationTolerance;
+    double rotationTolerance;
+    double maxTranslationSpeed;
+    double maxRotationSpeed;
+
+    public Waypoint(Pose2d pose,double translationTolerance,double rotationTolerance,double maxTranslationSpeed,double maxRotationSpeed){
+        this.pose = pose;
+        this.translationTolerance = translationTolerance;
+        this.rotationTolerance = rotationTolerance;
+        this.maxTranslationSpeed = maxTranslationSpeed;
+        this.maxRotationSpeed = maxRotationSpeed;
     }
     
-    
+    /**
+     * Checks if the robot is within the bounds of the waypoint.
+     * @param robotPose - The current pose of the robot.
+     */
     boolean isAtWaypoint(Pose2d robotPose){
-        if (robotPose < pose + translationTolerance || robotPose > pose - translationTolerance){
-            
+
+        if (robotPose.getX() < (pose.getX() + translationTolerance) || robotPose.getX() > (pose.getX() - translationTolerance) ){
+
+            if (robotPose.getY() < (pose.getY() + translationTolerance) || robotPose.getY() > (pose.getY() - translationTolerance) ){
+
+                return true;
+            }
         }
+        return false;
     }
 }
