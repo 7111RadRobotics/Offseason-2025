3d6352f7ba26824a28cc3bd0b8c921199be32203
Seamaj <seamajesty1st@gmail.com>
2025-07-15 16:23:14 -0500

    The pathmaster doc
    for setting the path stuff up correctly

---
+++ Library/src/main/java/frc/lib/pathfinding/PathMaster.java
@@ -0,0 +1,39 @@
+package frc.lib.pathfinding;
+
+import java.lang.reflect.Array;
+import java.util.function.Supplier;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+
+public class PathMaster {
+    PIDController pathPidController;
+    ProfiledPIDController profiledPathPidController;
+    Supplier<Pose2d> suppliedPose;
+    public PathMaster(PIDController pathPidController, ProfiledPIDController profiledPathPidController, Supplier<Pose2d> suppliedPose){
+        this.pathPidController = pathPidController;
+        this.profiledPathPidController = profiledPathPidController;
+        this.suppliedPose = suppliedPose;
+    }
+    public void setPID(double P, double I, double D){
+        pathPidController.setPID(P,I,D);
+    }
+    public void setProfliedPID(double P, double I, double D){
+        profiledPathPidController.setPID(P, I, D);
+    }
+    public void setMotionProfile(TrapezoidProfile trapezoidProfile){
+
+    }
+    public void setFieldElementMap(FieldElement[] fieldElementArray){
+        
+    }
+    public ChassisSpeeds getPathSpeeds(Path path, boolean avoidFieldElements, boolean simonmayremember){
+
+    }
+    public ChassisSpeeds getPathSpeedsProfiled(Path path, boolean avoidFieldElements, boolean simonmayremember){
+
+    }
+}
