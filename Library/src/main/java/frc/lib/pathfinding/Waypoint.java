f40ad16420dea0955d17aa1b59b51ab6c5cbb89c
Seamaj <seamajesty1st@gmail.com>
2025-07-15 15:51:53 -0500

    updated waypoint

--- Library/src/main/java/frc/lib/pathfinding/Waypoint.java
+++ Library/src/main/java/frc/lib/pathfinding/Waypoint.java
@@ -1,5 +1,19 @@
 package frc.lib.pathfinding;
 
+import edu.wpi.first.math.geometry.Pose2d;
+
 public class Waypoint {
+        Pose2d pose = pos;
+        double translationTolerance = transTolerance;
+        double rotationTolerance = rotTolerance;
+        double maxTranslationSpeed = maxTransSpeed;
+        double maxRotationSpeed = maxRotSpeed;
+    }
     
+    
+    boolean isAtWaypoint(Pose2d robotPose){
+        if (robotPose < pose + translationTolerance || robotPose > pose - translationTolerance){
+            
+        }
+    }
 }
