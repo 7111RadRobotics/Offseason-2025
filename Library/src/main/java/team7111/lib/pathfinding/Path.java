829cae4ea942b38267700fc792043c7813653138
Noybs125 <129349581+Noybs125@users.noreply.github.com>
2025-07-22 15:27:28 -0500

    no longer subsystem

--- Library/src/main/java/team7111/lib/pathfinding/Path.java
+++ Library/src/main/java/team7111/lib/pathfinding/Path.java
@@ -5,7 +5,7 @@ import java.util.function.Supplier;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class Path extends SubsystemBase {
+public class Path {
 
     //Robot position as a supplied Pose2d
     private Supplier<Pose2d> robotPose;
