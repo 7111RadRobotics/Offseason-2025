19b8e5fa97404f487d1423d6a8b374c36fa94330
Firereader06 <benjjackson05@outlook.com>
2025-10-21 19:47:35 -0500

    Yes, barrel is better

--- OffSeasonProject/src/main/java/team7111/robot/subsystems/Barrel.java
+++ OffSeasonProject/src/main/java/team7111/robot/subsystems/Barrel.java
@@ -2,9 +2,9 @@ package team7111.robot.subsystems;
 
 public class Barrel {
 
-    private barrelStates state = barrelStates.defaultState;
+    private BarrelStates state = BarrelStates.defaultState;
 
-    public enum barrelStates {
+    public enum BarrelStates {
         intake,
         adjust,
         readjust,
@@ -15,10 +15,70 @@ public class Barrel {
         defaultState
     };
 
-    public void setState(barrelStates state) {
+    public void setState(BarrelStates state) {
         this.state = state;
     }
 
+    private void manageState() {
+        switch(state) {
+            case intake:
+                intakeMethod();
+            break;
+
+            case adjust:
+                adjustMethod();
+            break;
+
+            case readjust:
+                readjustMethod();
+            break;
+
+            case shoot:
+                shootMethod();
+            break;
+
+            case reverse:
+                reverseMethod();
+            break;
+
+            case unload:
+                unloadMethod();
+            break;
+
+            case loaded:
+                loadedMethod();
+            break;
+
+            case defaultState:
+                defaultStateMethod();
+            break;
+        }
+    }
+
     public void periodic() {
+    }
+
+    private void intakeMethod() {
+    }
+
+    private void adjustMethod() {
+    }
+
+    private void readjustMethod() {
+    }
+
+    private void shootMethod() {
+    }
+
+    private void reverseMethod() {
+    }
+
+    private void unloadMethod() {
+    }
+
+    private void loadedMethod() {
+    }
+
+    private void defaultStateMethod() {
     }
 }
