f53fc7ebe256b6acd62b0ce273a2dd664b041f9e
Isaac4655 <157089689+Isaac4655@users.noreply.github.com>
2025-11-06 19:06:58 -0600

    Neatened Barrel, added comments

--- OffSeasonProject/src/main/java/team7111/robot/subsystems/Barrel.java
+++ OffSeasonProject/src/main/java/team7111/robot/subsystems/Barrel.java
@@ -26,36 +26,6 @@ import yams.motorcontrollers.local.SparkWrapper;
 
 public class Barrel extends SubsystemBase {
 
-    SparkMax barrelMotor = new SparkMax(1, MotorType.kBrushless);
-
-    private BarrelStates state = BarrelStates.defaultState;
-
-    private DigitalInput beamBreak = new DigitalInput(1);
-
-    public boolean beamBrakeState = false;
-
-    private String[] barrelGear = {"1","1"};
-
-    private GearBox barrelGearBox = new GearBox(barrelGear);
-
-    private MechanismGearing barrelGearing = new MechanismGearing(barrelGearBox);
-
-    private SmartMotorControllerConfig barrelMotorConfig = new SmartMotorControllerConfig(this)
-        .withControlMode(ControlMode.CLOSED_LOOP)
-        .withClosedLoopRampRate(Seconds.of(0.25))
-        .withSoftLimit(Degrees.of(0), Degrees.of(180))
-        .withGearing(barrelGearing);
-
-    private SmartMotorController barrelSparkController = new SparkWrapper(barrelMotor, DCMotor.getNEO(1), barrelMotorConfig);
-
-    private final FlyWheelConfig barrelConfig = new FlyWheelConfig(barrelSparkController)
-        .withDiameter(Inches.of(4))
-        .withMass(Pounds.of(.5))
-        .withUpperSoftLimit(RPM.of(1000))
-        .withTelemetry("IntakeConfig", TelemetryVerbosity.HIGH);
-
-    private FlyWheel barrel = new FlyWheel(barrelConfig);
-
     public enum BarrelStates {
         intake,
         adjust,
@@ -67,75 +37,115 @@ public class Barrel extends SubsystemBase {
         defaultState
     };
 
-    public void setState(BarrelStates state) {
-        this.state = state;
-    }
-
+    private DigitalInput beamBreak = new DigitalInput(1);
+    public boolean beamBrakeState = false;
     public boolean getBeamBrake() {
         return beamBreak.get();
+    }
+    
+    private SparkMax barrelMotor = new SparkMax(1, MotorType.kBrushless);
+    private String[] barrelGear = {"1","1"};
+    private GearBox barrelGearBox = new GearBox(barrelGear);
+    private MechanismGearing barrelGearing = new MechanismGearing(barrelGearBox);
+    private SmartMotorControllerConfig barrelMotorConfig = new SmartMotorControllerConfig(this)
+        .withControlMode(ControlMode.CLOSED_LOOP)
+        .withClosedLoopRampRate(Seconds.of(0.25))
+        .withSoftLimit(Degrees.of(0), Degrees.of(180))
+        .withGearing(barrelGearing);
+    
+    private SmartMotorController barrelSparkController = new SparkWrapper(barrelMotor, DCMotor.getNEO(1), barrelMotorConfig);
+    private final FlyWheelConfig barrelConfig = new FlyWheelConfig(barrelSparkController)
+        .withDiameter(Inches.of(4))
+        .withMass(Pounds.of(.5))
+        .withUpperSoftLimit(RPM.of(1000))
+        .withTelemetry("IntakeConfig", TelemetryVerbosity.HIGH);
+
+    private FlyWheel barrel = new FlyWheel(barrelConfig);
+
+    
+    private BarrelStates state = BarrelStates.defaultState;
+
+    public void setState(BarrelStates state) {
+        this.state = state;
     }
 
     private void manageState() {
         switch(state) {
             case intake:
-                intakeMethod();
+                intake();
                 break;
             case adjust:
-                adjustMethod();
+                adjust();
                 break;
             case readjust:
-                readjustMethod();
+                readjust();
                 break;
             case shoot:
-                shootMethod();
+                shoot();
                 break;
             case reverse:
-                reverseMethod();
+                reverse();
                 break;
             case unload:
-                unloadMethod();
+                unload();
                 break;
             case loaded:
-                loadedMethod();
+                loaded();
                 break;
             case defaultState:
-                defaultStateMethod();
+                defaultState();
                 break;
         }
     }
 
+    @Override
     public void periodic() {
+        manageState();
+        barrel.updateTelemetry();
     }
 
-    private void intakeMethod() {
+    @Override
+    public void simulationPeriodic() {
+        barrel.simIterate();
+    }
+
+    private void intake() {
+        // Placeholder values. Wheels active
         barrel.setSpeed(DegreesPerSecond.of(60));
     }
 
-    private void adjustMethod() {
-
+    private void adjust() {
+        //  Placeholder values. Moves wheels backward slowly
+        barrel.setSpeed(DegreesPerSecond.of(-40));
     }
 
-    private void readjustMethod() {
-
+    private void readjust() {
+        //  Placeholder values. Moves wheels forward
+        barrel.setSpeed(DegreesPerSecond.of(-80));
     }
 
-    private void shootMethod() {
+    private void shoot() {
+        // Placeholder values. Wheels will be active at full speed
         barrel.setSpeed(DegreesPerSecond.of(180));
     }
 
-    private void reverseMethod() {
+    private void reverse() {
+        // Placeholder values. Sets wheels reversing slowly
         barrel.setSpeed(DegreesPerSecond.of(-60));
     }
 
-    private void unloadMethod() {
+    private void unload() {
+        //  unloads the game piece, wheels active at low speeds
 
     }
 
-    private void loadedMethod() {
+    private void loaded() {
+        // Game piece is stored, wheels inactive
 
     }
 
-    private void defaultStateMethod() {
+    private void defaultState() {
+        //  Wheels inactive
         barrel.setSpeed(DegreesPerSecond.of(0));
     }
 }
