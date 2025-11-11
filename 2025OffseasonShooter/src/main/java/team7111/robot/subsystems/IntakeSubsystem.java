52c91c494ed18fca27cf652c2e2dd2dd8c02d736
Noybs125 <129349581+Noybs125@users.noreply.github.com>
2025-11-11 16:20:59 -0600

    added comments to intake to help with organizing other subsystems

--- 2025OffseasonShooter/src/main/java/team7111/robot/subsystems/IntakeSubsystem.java
+++ 2025OffseasonShooter/src/main/java/team7111/robot/subsystems/IntakeSubsystem.java
@@ -5,16 +5,12 @@ import com.revrobotics.spark.SparkLowLevel.MotorType;
 
 import static edu.wpi.first.units.Units.Amps;
 import static edu.wpi.first.units.Units.Degrees;
-import static edu.wpi.first.units.Units.DegreesPerSecond;
-import static edu.wpi.first.units.Units.DegreesPerSecondPerSecond;
 import static edu.wpi.first.units.Units.Inches;
 import static edu.wpi.first.units.Units.Seconds;
 import static edu.wpi.first.units.Units.Pounds;
 import static edu.wpi.first.units.Units.RPM;
 
 import edu.wpi.first.math.system.plant.DCMotor;
-import edu.wpi.first.units.measure.AngularVelocity;
-import edu.wpi.first.units.measure.Velocity;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import yams.gearing.GearBox;
 import yams.gearing.MechanismGearing;
@@ -30,7 +26,7 @@ import yams.mechanisms.config.FlyWheelConfig;
 import yams.mechanisms.velocity.FlyWheel;
 
 public class IntakeSubsystem extends SubsystemBase{
-
+    // enums defined above variables
     public enum IntakeState {  
         store,
         defualtState,
@@ -40,6 +36,9 @@ public class IntakeSubsystem extends SubsystemBase{
         manual,
     };
 
+    // variables defined above methods
+
+    // variables relating to intake flywheels
     private SparkMax flywheelMotor = new SparkMax(1, MotorType.kBrushless);
     private MechanismGearing flywheelGearing = new MechanismGearing(GearBox.fromReductionStages(1.2, 1));
     private SmartMotorControllerConfig flywheelMotorConfig = new SmartMotorControllerConfig(this)
@@ -55,12 +54,12 @@ public class IntakeSubsystem extends SubsystemBase{
     private FlyWheelConfig intakeConfig = new FlyWheelConfig(flywheelController)
         .withDiameter(Inches.of(2.25))
         .withMass(Pounds.of(0.5))
-        .withUpperSoftLimit(RPM.of(1000))
+        .withUpperSoftLimit(RPM.of(4000))
         .withTelemetry("IntakeFlywheels", TelemetryVerbosity.HIGH);
 
     private FlyWheel flywheels = new FlyWheel(intakeConfig);
 
-
+    // variables relating to intake pivot
     private SparkMax pivotMotor = new SparkMax(2, MotorType.kBrushless);
     private MechanismGearing pivotGearing = new MechanismGearing(GearBox.fromReductionStages(18, 1));
     private SmartMotorControllerConfig pivotMotorConfig = new SmartMotorControllerConfig(this)
@@ -82,12 +81,34 @@ public class IntakeSubsystem extends SubsystemBase{
 
     private Pivot pivot = new Pivot(pivotConfig);
 
-
+    // other variables
     private IntakeState state = IntakeState.defualtState;
 
+    private double currentPivotSetpoint = 0;
+    private double currentFlywheelSpeed = 0;
+
+    // methods defined at bottom
+    // class constructor above all other methods
     public IntakeSubsystem() {}
 
+    // periodic and simulationPeriodic just below constructor
+    @Override
+    public void periodic() {
+        manageState();
+        if(pivot.getMechanismSetpoint().isPresent())
+            currentPivotSetpoint = pivot.getMechanismSetpoint().get().in(Degrees);
+        currentFlywheelSpeed = flywheels.getSpeed().in(RPM);
+        flywheels.updateTelemetry();
+        pivot.updateTelemetry();
+    }
 
+    @Override
+    public void simulationPeriodic() {
+        flywheels.simIterate();
+        pivot.simIterate();
+    }
+
+    // all subsytems that contain a state enum must contain a getState, setState, manageState, and methods for each state
     public void setState(IntakeState state){
         this.state = state;
     }
@@ -119,44 +140,41 @@ public class IntakeSubsystem extends SubsystemBase{
         }
     }
 
-    @Override
-    public void periodic() {
-        manageState();
-        flywheels.updateTelemetry();
-        pivot.updateTelemetry();
-    }
-
-    @Override
-    public void simulationPeriodic() {
-        flywheels.simIterate();
-        pivot.simIterate();
-    }
-
     private void store() {
         // pivot will be inside and wheels stopped
-        flywheels.setSpeed(RPM.of(0)).execute();
-        pivot.setAngle(Degrees.of(0));
+        flywheels.setSpeed(RPM.of(0)).execute(); // execute must be called if the method is a command
+        pivot.setAngle(Degrees.of(0)).execute();
     }
 
     private void deploy() {
         // placeholder values. pivot will be extended and wheels intaking
         flywheels.setSpeed(RPM.of(1000)).execute();
-        pivot.setAngle(Degrees.of(48));
+        pivot.setAngle(Degrees.of(48)).execute();
     }
 
     private void transition() {
         // placeholder values. pivot will be inside and wheels intaking
-        flywheels.setSpeed(RPM.of(1000));
-        pivot.setAngle(Degrees.of(0));
+        flywheels.setSpeed(RPM.of(1000)).execute();
+        pivot.setAngle(Degrees.of(0)).execute();
     }
 
     private void eject() {
         // placeholder values. pivot will be extended and wheels reversing
         flywheels.setSpeed(RPM.of(-1000)).execute();
-        pivot.setAngle(Degrees.of(48));
+        pivot.setAngle(Degrees.of(48)).execute();
     }
 
     private void manual() {
         
+    }
+
+    // other methods at the bottom
+
+    public void setManual(double pivotIncrement, double flywheelSpeed){
+        state = IntakeState.manual;
+        currentFlywheelSpeed = flywheelSpeed;
+        currentPivotSetpoint += pivotIncrement;
+        flywheels.set(flywheelSpeed).execute();
+        pivot.setAngle(Degrees.of(currentPivotSetpoint)).execute();
     }
 }
